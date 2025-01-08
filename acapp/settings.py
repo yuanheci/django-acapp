157a21a3b3ef6046ffeebab7b957571375d30ac4
rsh <rsh15355756202@163.com>
2025-01-08 13:39:39 +0800

    write my first page

--- acapp/settings.py
+++ acapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2gooqa3t!5v*hrk=gt-h*nbxkps&#=%%yk*gfet6+n%d!%@#wr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["192.144.200.235"]
 
 
 # Application definition
