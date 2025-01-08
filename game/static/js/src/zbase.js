0a8a8fa17ffee519c03a6f44a3fa7198d9f7c5ac
rsh <rsh15355756202@163.com>
2025-01-08 20:33:12 +0800

    write menu and playground scene

--- game/static/js/src/zbase.js
+++ game/static/js/src/zbase.js
@@ -2,8 +2,8 @@ class AcGame{
     constructor(id) {
         this.id = id;
         this.$ac_game = $('#' + id); 
-        this.menu = new AcGameMenu(this)
-        this.palyground = new AcGamePlayground(this);
+        this.menu = new AcGameMenu(this);
+        this.playground = new AcGamePlayground(this);
 
         this.start();
     }
